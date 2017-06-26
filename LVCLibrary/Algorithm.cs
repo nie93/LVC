a1766405e4035efa83b5262bf9df86491b4abb16
Zhijie Nie <nie@vt.edu>
2017-06-26 11:13:00 -0400

    Solved Tap_changer_value issue

--- LVCLibrary/Algorithm.cs
+++ LVCLibrary/Algorithm.cs
@@ -1,13 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
 using System.Text;
 using ECAClientFramework;
 using ECAClientUtilities.API;
+using ECACommonUtilities;
+using ECACommonUtilities.Model;
 using LVC.Model.LVCData;
+using LVC.Adapters;
+using LVC.VcControlDevice;
+using GSF.Configuration;
+using GSF.TimeSeries;
+using GSF.TimeSeries.Transport;
 
 namespace LVC
 {
     public static class Algorithm
     {
+        static VoltVarControllerAdapter vca;
+        static VoltVarController PreviousFrame;
+
+        static Algorithm()
+        {
+            /*
+             * Testing Files Configurations
+             * test1.xml    -   Verify if the controller can RAISE both transformers' taps when voltages on both buses are lower than the limit (VLLIM = 114.5kV)
+             * test2.xml    -   Verify if the controller is still able to operate (VLLIM = 114.5kV), when the other transformer's tap has reached the highest tap position (16)
+             * test3.xml    -   Verify if the controller can switch ON the capacitor bank when the voltage in Pamplin substation reach the lower limit (Clov = 113.5kV)
+             * test4.xml    -   Verify if the controller can switch OFF the capacitor bank when the voltage in Crewe substation reach the higher limit (Chiv = 119.7kV)
+            */
+            string configurationPathName = (@"C:\Users\niezj\Documents\dom\LVC\Data\Configurations_test1.xml");
+            vca = new VoltVarControllerAdapter();
+            vca.ConfigurationPathName = configurationPathName;
+            vca.Initialize();
+            PreviousFrame = new VoltVarController();
+            PreviousFrame = VoltVarController.DeserializeFromXml(configurationPathName);
+        }
         public static Hub API { get; set; }
 
         internal class Output
@@ -31,6 +62,28 @@ namespace LVC
         {
             Output output = Output.CreateNew();
 
+            #region [ Environment Settings ]
+            string MainFolderPath = (@"C:\Users\niezj\Documents\dom\LVC\");
+            string DataFolderPath = Path.Combine(MainFolderPath, @"Data");
+            string LogFolderPath = Path.Combine(MainFolderPath, @"Log");
+
+            string[] LogFileNameList = Directory.GetFiles(LogFolderPath, "CtrlDecisionLog_*.xml");
+            string LogFileName = $"CtrlDecisionLog_{LogFileNameList.Length + 1:000}_{DateTime.UtcNow:yyyyMMdd_HHmmssfff}.xml";
+
+            string LogFilePath = Path.Combine(LogFolderPath, LogFileName);
+
+            bool EnableVoltageControlsMessageOutput = true;
+            #endregion
+
+            // Extract inputData from openECA then Call SubRoutine
+            vca.GetData(inputData, inputMeta, PreviousFrame);
+
+            // Logging control decision to *.xml files
+            vca.SerializeToXml(LogFilePath);
+
+            // Store Current vca.InputFrame to previous.InputFrame for the next InputFrame 
+            PreviousFrame = vca.InputFrame;
+
             try
             {
                 // TODO: Implement your algorithm here...
@@ -38,33 +91,40 @@ namespace LVC
 
                 output.OutputData.IntOutput = (Int16)DateTime.Now.Millisecond;
 
-                #region [ Write openECA Client Windows Message ]
-                StringBuilder _message = new StringBuilder();
-                _message.Append($"\n ================= LVC_test Analytics ================");
-                _message.Append($"\n                 Run Time:  {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
-                _message.Append($"\n -----------------------------------------------------");
-                _message.Append($"\n                  TapVTx4:  {inputData.TapVTx4}");
-                _message.Append($"\n                  TapVTx5:  {inputData.TapVTx5}");
-                _message.Append($"\n              CapBkrVCap1:  {inputData.CapBkrVCap1}");
-                _message.Append($"\n              CapBkrVCap2:  {inputData.CapBkrVCap2}");
-                _message.Append($"\n              BusBkrVCap1:  {inputData.BusBkrVCap1}");
-                _message.Append($"\n              BusBkrVCap2:  {inputData.BusBkrVCap2}");
-                _message.Append($"\n                VoltsVTx4:  {inputData.VoltsVTx4:0.000} Volts");
-                _message.Append($"\n                VoltsVTx5:  {inputData.VoltsVTx5:0.000} Volts");
-                _message.Append($"\n               LocKvVCap1:  {inputData.LocKvVCap1:0.000} Volts");
-                _message.Append($"\n               LocKvVCap2:  {inputData.LocKvVCap2:0.000} Volts");
-                _message.Append($"\n                   MwVTx4:  {inputData.MwVTx4:0.000} MW");
-                _message.Append($"\n                  MvrVTx4:  {inputData.MvrVTx4:0.000} MVAR");
-                _message.Append($"\n                   MwVTx5:  {inputData.MwVTx5:0.000} MW");
-                _message.Append($"\n                  MvrVTx5:  {inputData.MvrVTx5:0.000} MVAR");
-                _message.Append($"\n                     G1Mw:  {inputData.G1Mw:0.000} MW");
-                _message.Append($"\n                    G1Mvr:  {inputData.G1Mvr:0.000} MVAR");
-                _message.Append($"\n                     G2Mw:  {inputData.G2Mw:0.000} MW");
-                _message.Append($"\n                    G2Mvr:  {inputData.G2Mvr:0.000} MVAR");
-                _message.Append($"\n =====================================================");
-                _message.Append($"\n            FakeIntOutput:  {output.OutputData.IntOutput:0.000}");
-                MainWindow.WriteMessage(_message.ToString());
+                #region [ Write openECA Client Windows Message]
+                if (EnableVoltageControlsMessageOutput)
+                {
+                    StringBuilder _message = new StringBuilder();
+                    _message.AppendLine($" =================== LVC Analytics ===================");
+                    _message.AppendLine($"                     Run Time:  {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
 
+                    _message.AppendLine($" ------------- Control Decision Message ------------- ");
+                    _message.AppendLine($" {vca.LogMessage.Replace("|", "\n\t")}");
+
+                    _message.AppendLine($" -------------- Contrlled Transformers -------------- ");
+                    foreach (VcTransformer Transformer in vca.InputFrame.ControlTransformers)
+                    {
+                        _message.AppendLine($"  Transformer    DeviceId:  {Transformer.DeviceId}");
+                        _message.AppendLine($"                 LtcCtlId:  {Transformer.LtcCtlId}");
+                        _message.AppendLine($"                     TapV:  {Transformer.TapV}");
+                        _message.AppendLine($"                   VoltsV:  {Transformer.VoltsV}");
+                    }
+
+                    _message.AppendLine($" ------------ Controlled Capacitor Banks ------------ ");
+                    foreach (VcCapacitorBank CapBank in vca.InputFrame.ControlCapacitorBanks)
+                    {
+                        _message.AppendLine($"  CapacitorBank  CapBkrId:  {CapBank.CapBkrId}");
+                        _message.AppendLine($"                  CapBkrV:  {CapBank.CapBkrV}");
+                        _message.AppendLine($"                   LockvV:  {CapBank.LockvV}");
+                        _message.AppendLine($"                     Clov:  {CapBank.Clov}");
+                        _message.AppendLine($"                     Chiv:  {CapBank.Chiv}");
+                    }
+
+                    _message.AppendLine($" =====================================================");
+
+                    MainWindow.WriteMessage(_message.ToString());
+                }
+                
                 #endregion
             }
             catch (Exception ex)
